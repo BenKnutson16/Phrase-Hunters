c5e75c0ac5c5ddc40dba9539d12dfa1671b87e67
BenKnutson16 <103281247+BenKnutson16@users.noreply.github.com>
2022-06-13 21:30:55 -0400

    phrase hunter game

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from game import Game
-from phrase import Phrase
+from phrasehunters import game
+from phrasehunters import phrase
 
 if __name__ == '__main__':
     restart = True
     while restart:
-        game = Game()
-        game.start()
-        play_again = input("Would you like to play again? (Y/N)")
+        play_game = game.Game()
+        play_game.start()
+        play_again = input("\nWould you like to play again? (Y/N) ")
         if play_again.lower() == 'y':
             restart = True
         else:
